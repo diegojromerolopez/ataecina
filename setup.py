d9c157028f33f2255bbd55572b152a692362ab03
Diego J <diegojromerolopez@gmail.com>
2021-05-27 01:32:33 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-path = os.path.abspath(__file__)
+root_dir_path = os.path.dirname(os.path.abspath(__file__))
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert("README.md", "rst")
 except(IOError, ImportError):
-    long_description = open(os.path.join(path, 'README.md')).read()
+    long_description = open(os.path.join(root_dir_path, "README.md")).read()
 
-data_files = []
-for dirpath, dirnames, filenames in os.walk(path):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        continue
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-with open(os.path.join(path, "requirements.txt")) as requirements_file:
+with open(os.path.join(root_dir_path, "requirements.txt")) as requirements_file:
     requirements = requirements_file.readlines()
 
 setup(
@@ -30,14 +20,19 @@ setup(
     description="A simple image converter to art.",
     long_description=long_description,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        "Development Status :: 3 - Alpha",
         'License :: OSI Approved :: MIT License',
     ],
     install_requires=requirements,
     license="MIT",
     keywords="images pillow svg converter",
     url='https://github.com/diegojromerolopez/atecina',
-    packages=find_packages(path),
-    data_files=data_files,
+    packages=["converters"],
+    package_dir={"converters": "src/converters"},
+    data_files=[],
     include_package_data=True,
+    scripts=[
+        "bin/random_circler.py",
+        "bin/mount_mongofs.py"
+    ]
 )
